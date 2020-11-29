9050977a054c03bae8fb0b6d2240d05765def905
AhmedNasrElhariri <49247638+AhmedNasrElhariri@users.noreply.github.com>
2020-11-29 18:09:37 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const mongoConnect = require('./util/database').mongoConnect;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./roots/admin');
+const matchRoutes = require('./roots/match');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin',adminRoutes);
+app.use(matchRoutes);
+
+
+mongoConnect(() => {
+    console.log('Connnn');
+  app.listen(3000);
+});
